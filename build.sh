0b265cbc420577938e7a4882f021bfdc11bebe22
Luke Wallin <luke@lukewallin.co.uk>
2020-08-11 12:21:18 +0100

    first stab at a deb package

---
+++ build.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+mkdir -p build/usr/share/h264-live-player
+mkdir -p build/etc/systemd/system
+
+cp -r lib build/usr/share/h264-live-player/
+cp -r public build/usr/share/h264-live-player/
+cp -r samples build/usr/share/h264-live-player/
+cp -r vendor build/usr/share/h264-live-player/
+cp -r *.js build/usr/share/h264-live-player/
+cp -r DEBIAN build/
+cp h264-live-player.service build/etc/systemd/system/
+
+mkdir -p out
+
+VERSION="$(git describe)"
+
+sed -i "s/VERSIONHERE/${VERSION}/g" build/DEBIAN/control
+
+fakeroot dpkg-deb -b build out
+
+rm -r build
